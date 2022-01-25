00f406340de991b7b78a2d93f61f9049d47e176a
reginpv <reginpv@gmail.com>
2022-01-26 05:08:30 +0800

    update UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,21 @@
 const LOCATIONS = [
   {
     label: "a",
+    name: "Guiguinto",
     center: [14.78708, 120.88335],
     marker: [14.82749, 120.88414],
     zoom: 13
   }, 
   {
     label: "b",
+    name: "Makati",
     center: [14.53424, 121.02156],
     marker: [14.55381, 121.01912],
     zoom: 13
   },
   {
     label: "init",
+    name: "All",
     center: [9.796, 122.080],
     zoom: 6
   }
@@ -32,24 +35,37 @@ map.addLayer(layer)
 
 // Create markers
 LOCATIONS.forEach(loc=>{
-
   if(loc.marker) {
     let marker = L.marker(loc.marker)
     marker.addTo(map)
   }
-
 })
 
 // Map view updater
-function updateMap(term) {
+const mapButtons = document.querySelectorAll("[data-map]")
+mapButtons.forEach(button=>{
 
-  const activeView = LOCATIONS.filter(loc=>loc.label==term)
-  console.log(term, activeView)
+  button.addEventListener('click', function(e){
 
-  if(activeView.length>0) {
-    map.setView(activeView[0].center, activeView[0].zoom)
-  } else {
-    map.setView(mapOptions.center, mapOptions.zoom)
-  }
+    // Clean UI
+    mapButtons.forEach(b=>b.classList.remove("active"))
 
-}
+    // Set vars
+    let target = this
+    let value = target.getAttribute("data-map")
+
+    // Set active
+    const activeView = LOCATIONS.filter(loc=>loc.label==value)
+
+    // Update view
+    if(activeView.length>0) {
+      map.setView(activeView[0].center, activeView[0].zoom)
+    } else {
+      map.setView(mapOptions.center, mapOptions.zoom)
+    }
+
+    // Update ui
+    target.classList.add("active")
+
+  })
+})
